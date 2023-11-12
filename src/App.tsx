6d99279e588918b5f6403c085069b500a4f91fdf
Utkarsh Chugh <utkarshchugh@Utkarshs-MacBook-Pro.local>
2023-11-12 15:18:35 +0530

    TGH-2: Setting up Basic UI structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './components/Home';
+import { ChakraProvider } from '@chakra-ui/react'
+import { ErrorBoundary } from './components/common/ErrorBoundary';
+
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+      <ErrorBoundary>
+        <ChakraProvider>
+          <div className="App">
+            <Home/>
+            </div>
+        </ChakraProvider>
+      </ErrorBoundary>
   );
 }
 
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,46 @@
-import React, { useState } from 'react'
-import { Input } from '@chakra-ui/react'
+import React, { useEffect, useState, createContext } from 'react'
+import { Input, Spinner } from '@chakra-ui/react'
+import { getSearchContent } from './hooks/getSearchContent'
+import { ResponseType } from './types/Types'
+import { Content } from './content/components/Content'
+import { ErrorMessage } from './common/ErrorMessage'
+import { useDebounce } from './common/utils/useDebounce'
+import { NavigationTabs } from './home/NavigationTabs'
+import { tabValues } from './home/tabValues'
 
 export const Home = () => {
+    const [data, setData] = useState<ResponseType | undefined>()
     const [searchVal, setSearchVal] = useState('')
+    const [debounce, setDebounce] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string>('')
+
+    const [tabIndex, setIndex] = useState<number>(0)
+    const DataProvider = createContext(data)
 
     const handleChange = (e: any) => {
         setSearchVal(e.target.value)
     }
+    
+    useDebounce(searchVal, setDebounce, 300);
+
+    useEffect(() => {
+        const tabVal = tabValues[tabIndex]
+        getSearchContent(debounce, tabVal, setData,  setLoading, setError)
+    }, [debounce, tabIndex])
+
+    useEffect(() => {
+        console.log('Data is', data)
+    }, [data])
     return (
-        <div><Input placeholder='Search Content' type='text' size='md' value={searchVal} onChange={handleChange}/></div>
+        <div className='homeContainer'>
+
+            <div className='inputContainer'>
+                <Input placeholder='Search Content' w='400px' color='white' backgroundColor='gray.700' type='text' size='md' value={searchVal} onChange={handleChange} />
+            </div>
+            <div className='bodyContainer'>
+                <NavigationTabs tabIndex={tabIndex} setTabIndex={setIndex} data={data} loading={loading} error={error} />
+            </div>
+        </div>
     )
 }
