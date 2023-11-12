a23da843fc495fdacbeb43aa492c6f7a898ae377
Utkarsh Chugh <utkarshchugh98@gmail.com>
2023-11-12 15:30:01 +0530

    TGH - 4 : Adding useDebounce

---
+++ src/components/common/utils/useDebounce.ts
@@ -0,0 +1,13 @@
+import { useEffect } from "react"
+
+export const useDebounce = (value: any, setValue: Function, timer: number) => {
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setValue(value)
+        }, timer)
+        return () => {
+            clearTimeout(timerId)
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [value])
+}
