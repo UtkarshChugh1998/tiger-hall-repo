cd675119c64fef5c61f137b5a88279d6d3976e65
Utkarsh Chugh <utkarshchugh98@gmail.com>
2023-11-13 15:37:00 +0530

    TGH -11 : Test case to check render App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders Ebook Tab link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/Ebook/i)
+  expect(linkElement).toBeInTheDocument()
+})
+test('renders Podcast Tab link', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/Podcast/i)
   expect(linkElement).toBeInTheDocument()
 })
