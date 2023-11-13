7f2ac0ff8ce78491345ebe202a99aa54f5bca87f
Utkarsh Chugh <utkarshchugh98@gmail.com>
2023-11-14 00:45:27 +0530

    TGH - 12: Adding notification for no results

--- src/components/content/components/Content.tsx
+++ src/components/content/components/Content.tsx
@@ -3,6 +3,7 @@ import { ContentCards, Error } from '../../types/Types'
 import { ContentCard } from './ContentCard'
 import '../styles/styles.css'
 import { ErrorMessage } from '../../common/ErrorMessage'
+import { Alert, AlertIcon } from '@chakra-ui/react'
 type IContentProps = {
   data?: ContentCards
   type: string
@@ -13,9 +14,18 @@ export const Content = (props: IContentProps) => {
   return (
     <div className="contentContainer">
       {error && error.length > 0 && <ErrorMessage error={error[0].message} />}
-      {data?.edges?.map((edge, index) => {
-        return <ContentCard key={index} data={edge} type={type} />
-      })}
+      {data?.edges.length === 0 ? (
+        <Alert status="warning" color="black">
+          <AlertIcon />
+          Oops! No results, please refine your search
+        </Alert>
+      ) : (
+        <>
+          {data?.edges?.map((edge, index) => {
+            return <ContentCard key={index} data={edge} type={type} />
+          })}
+        </>
+      )}
     </div>
   )
 }
